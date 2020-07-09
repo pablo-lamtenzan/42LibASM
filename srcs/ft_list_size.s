5d07b087340e68581da5356700856287e6248fb9
Pablo Lamtenzan <pablolamtenzan@gmail.com>
2020-07-09 16:30:57 +0200

    test

--- srcs/ft_list_size.s
+++ srcs/ft_list_size.s
@@ -1,23 +1,23 @@
 global ft_list_size
 section .text
 
-/*
-ft_list_size:
-	push		rbp
-	mov			rbp, rsp
-	xor			rax, rax
 
-.loop:
-	test		rdi, rdi
-	jz			.end
+;ft_list_size:
+;	push		rbp
+;	mov			rbp, rsp
+;	xor			rax, rax
+;
+.;loop:
+;	test		rdi, rdi
+;	jz			.end
+;
+;	mov			rdi, [rdi + 8]
+;	inc			rax
+;	jmp			.loop
 
-	mov			rdi, [rdi + 8]
-	inc			rax
-	jmp			.loop
+;.end:
+;	ret
 
-.end:
-	ret
-*/
 
 ft_list_size:					; RDI - RAX
 	sub		rax,	rax			; Clear RCX
